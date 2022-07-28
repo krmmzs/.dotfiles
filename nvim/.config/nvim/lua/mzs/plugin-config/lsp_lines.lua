3732f1106df5d8d023ebda89dd56fdee94c84bbc
mouzaisi <mouzaisibyl@gmail.com>
2022-07-29 03:06:06 +0800

    Keymap to switch virtual text

--- nvim/.config/nvim/lua/mzs/plugin-config/lsp_lines.lua
+++ nvim/.config/nvim/lua/mzs/plugin-config/lsp_lines.lua
@@ -1,4 +1,20 @@
 -- Disable virtual_text since it's redundant due to lsp_lines.
-vim.diagnostic.config({
-  virtual_text = false,
-})
+--vim.diagnostic.config({
+  --virtual_text = false,
+--})
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set(
+    "n",
+    "<leader>1l",
+    ":lua vim.diagnostic.config({virtual_text = true, virtual_lines = false})<CR>",
+    opts
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>2l",
+    ":lua vim.diagnostic.config({virtual_text = false, virtual_lines = true})<CR>",
+    opts
+)
